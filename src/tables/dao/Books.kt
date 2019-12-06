f4441542b30675c57528ab989527c9faeb937940
Shengyou Fan <shengyoufan@gmail.com>
2019-12-06 15:29:46 +0800

    clean up

--- src/tables/dao/Books.kt
+++ src/tables/dao/Books.kt
@@ -1,12 +1,10 @@
 package io.kraftsman.sample.tables.dao
 
 import org.jetbrains.exposed.dao.IntIdTable
-import org.jetbrains.exposed.sql.Column
-import org.joda.time.DateTime
 
 object Books : IntIdTable() {
-    val title: Column<String> = varchar("title", 255)
-    val isbn: Column<String> = varchar("isbn", 13)
-    val publishedAt: Column<DateTime> = datetime("published_at")
+    val title = varchar("title", 255)
+    val isbn = varchar("isbn", 13)
+    val publishedAt = datetime("published_at")
     val author = reference("author", Authors)
 }
--- src/tables/dsl/Books.kt
+++ src/tables/dsl/Books.kt
@@ -1,14 +1,10 @@
 package io.kraftsman.sample.tables.dsl
 
-import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
-import org.joda.time.DateTime
 
 object Books : Table() {
-    val id: Column<Int> = integer("id")
-                            .autoIncrement()
-                            .primaryKey()
-    val title: Column<String> = varchar("title", 255)
-    val isbn: Column<String> = varchar("isbn", 13)
-    val publishedAt: Column<DateTime> = datetime("published_at")
+    val id = integer("id").autoIncrement().primaryKey()
+    val title = varchar("title", 255)
+    val isbn = varchar("isbn", 13)
+    val publishedAt = datetime("published_at")
 }
