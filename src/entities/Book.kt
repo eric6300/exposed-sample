81f3d61a861b34c843967155434605c94bb2b6a2
Shengyou Fan <shengyoufan@gmail.com>
2019-12-06 12:30:12 +0800

    add Book entity

---
+++ src/entities/Book.kt
@@ -0,0 +1,14 @@
+package io.kraftsman.sample.entities
+
+import io.kraftsman.sample.tables.dao.Books
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+
+class Book(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Book>(Books)
+
+    var title by Books.title
+    var isbn by Books.isbn
+    var publishedAt by Books.publishedAt
+}
