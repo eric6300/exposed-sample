fdb182b3c961177cb22ca889dbef45e6e7637c0d
Shengyou Fan <shengyoufan@gmail.com>
2019-12-06 15:21:32 +0800

    define relation many-to-one

--- src/entities/Author.kt
+++ src/entities/Author.kt
@@ -1,6 +1,7 @@
 package io.kraftsman.sample.entities
 
 import io.kraftsman.sample.tables.dao.Authors
+import io.kraftsman.sample.tables.dao.Books
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
@@ -9,4 +10,5 @@ class Author(id: EntityID<Int>): IntEntity(id) {
     companion object: IntEntityClass<Author>(Authors)
 
     var name by Authors.name
+    val books by Book referrersOn Books.author
 }
